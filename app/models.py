91c4540c1f154ae1fa67682b904bee9d9966698f
teju_2506 <tejaswiniskhairnar123@gmail.com>
2023-02-09 17:52:58 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# class Information(models.Model):
+#     firstname = models.CharField(max_length=255)
+#     lastname = models.CharField(max_length=255)
+#     email = models.EmailField()
+#     phone = models.IntegerField()
+     
+
+# Create your models here.
